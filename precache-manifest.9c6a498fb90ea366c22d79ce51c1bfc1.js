c1317f5a686cc1aeb13492fbaf486a219ee0ed59
armenbalikyan <armbalikyans@gmail.com>
2020-02-21 12:44:38 +0400

    Updates

---
+++ precache-manifest.9c6a498fb90ea366c22d79ce51c1bfc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e87aaf8f10e09cf0101a1e84f3294d",
+    "url": "/tictactoee/index.html"
+  },
+  {
+    "revision": "07315440b8f5fd580040",
+    "url": "/tictactoee/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "a55121069db4fb64af21",
+    "url": "/tictactoee/static/css/main.63e5bcee.chunk.css"
+  },
+  {
+    "revision": "07315440b8f5fd580040",
+    "url": "/tictactoee/static/js/2.29bcb332.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/tictactoee/static/js/2.29bcb332.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a55121069db4fb64af21",
+    "url": "/tictactoee/static/js/main.d26681c2.chunk.js"
+  },
+  {
+    "revision": "ef2800bad1c32660c0c6",
+    "url": "/tictactoee/static/js/runtime-main.c637c24d.js"
+  }
+]);
